764e02e411d33548da51a640c59d213d8b3ec4e7
Raju Pangeni <evit.programmer@gmail.com>
2017-03-08 14:22:14 +0545

    form added for answer input in index page

--- index.php
+++ index.php
@@ -9,9 +9,17 @@
 		<h1>Quiz Ninja!</h1>
 		<p>Score:<strong id="score">0</strong></p>
 	</header>
+
 	<section id="question"></section>
+
+	<form id="answer">
+		<input type="text">
+		<button>Submit Answer</button>
+	</form>
+
 	<section id="feedback"></section>
 	<button id="start">Click To Play Quiz Ninja!</button>
+	
 <script src="js/script.js"></script>
 </body>
 </html>
